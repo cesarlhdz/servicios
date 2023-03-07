d735400c05f4e32ff4be01ab04974f3deac68dd7
Cesar Luna <cesar.luna@iest.edu.mx>
2023-03-06 18:18:12 -0600

    Initial commit

---
+++ app/src/main/java/edu/iest/servicios/activity_segunda.kt
@@ -0,0 +1,11 @@
+package edu.iest.servicios
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_segunda : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+    }
+}
